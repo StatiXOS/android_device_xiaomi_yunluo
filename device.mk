ec1a1afb259a31a0153a4b5c3688ba7344bd9dfb
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    yunluo: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -63,9 +63,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Allow userspace reboots
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
-# Updatable APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Windows extensions
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
