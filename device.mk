f83014597983fd4941fc3245e343e55752224178
Pranaya Deomani <pranayadeomani@proton.me>
2024-04-10 19:05:00 +0530

    yunluo: Define preferred pixel density bucket
    Change-Id: I03b665b51a02b2bfd31354f62d4413d7d5d0bf80

--- device.mk
+++ device.mk
@@ -70,6 +70,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_SHIPPING_API_LEVEL := 31
 
 # AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_CHARACTERISTICS := tablet
 
 # Audio
