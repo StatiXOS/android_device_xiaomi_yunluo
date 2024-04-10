73eed687068487bd41041d943c537e82df48927a
Pranaya Deomani <pranayadeomani@proton.me>
2024-04-10 18:58:42 +0530

    yunluo: Add some build identifiers from stock
    Change-Id: I7a6433427cbe9f8ac9930ff3e9e7655f4245f1fe

--- lineage_yunluo.mk
+++ lineage_yunluo.mk
@@ -19,3 +19,10 @@ PRODUCT_DEVICE := yunluo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Pad
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="yunluo-user 12 SP1A.210812.016 V14.0.4.0.TLYMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V14.0.4.0.TLYMIXM:user/release-keys
