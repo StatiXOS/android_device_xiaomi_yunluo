16e0f79bc4a857f198027508a5549a512e0ed589
Pranaya Deomani <pranayadmn@proton.me>
2024-05-28 21:30:04 +0530

    yunluo: Import libinit from Xiaomi SM8250 and wire it up
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Change-Id: I7d0a0ea90b2c77f5e69fbc4ab3a4ea206fe7eaa4

---
+++ libinit/include/libinit_utils.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <string>
+
+void property_override(std::string prop, std::string value, bool add = true);
