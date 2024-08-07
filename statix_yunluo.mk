a45e0eada7d7c5777cb0d4dbb21d1b2a7e0eeb51
Pranaya Deomani <pranayadmn@proton.me>
2024-08-07 16:52:38 +0530

    yunluo: Update blobs from miui_YUNLUOGlobal_OS1.0.4.0.ULYMIXM
    Change-Id: I61e8f3c7a694e2db1e3d1baf773374c30b1cc5d1

--- statix_yunluo.mk
+++ statix_yunluo.mk
@@ -27,6 +27,6 @@ PRODUCT_MODEL := Redmi Pad
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="yunluo-user 12 SP1A.210812.016 V816.0.3.0.ULYMIXM release-keys"
+    PRIVATE_BUILD_DESC="yunluo-user 12 SP1A.210812.016 V816.0.4.0.ULYMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.3.0.ULYMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.4.0.ULYMIXM:user/release-keys
